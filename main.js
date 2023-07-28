28e24f08d178912eceea6ed9624e78139e2d6f7b
aftab <aft@motor.com>
2023-07-28 21:28:34 +0530

    First commit here

---
+++ main.js
@@ -0,0 +1,10 @@
+const submit = document.getElementById("select-hint");
+let hintContent = document.getElementById("info");
+
+submit.addEventListener("mouseover", function () {
+  hintContent.style.visibility = "visible";
+});
+
+submit.addEventListener("mouseout", function () {
+  hintContent.style.visibility = "hidden";
+});
